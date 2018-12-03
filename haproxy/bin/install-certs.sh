8f3879b35b0931809f7898f985ce7113ec93e18a
Hoonio <hoonio@hoonio.com>
2018-12-03 14:45:50 +0100

    backup code for docker cloud before migrating to kube

--- haproxy/bin/install-certs.sh
+++ haproxy/bin/install-certs.sh
@@ -8,7 +8,7 @@ mkdir -p "$LIVE_CERT_FOLDER"
 # Create a self signed default certificate, so HAproxy can start before we have
 # any real certificates.
 if [[ ! -f "$CERT_FOLDER/cert0.pem" ]]; then
-	openssl req -x509 -newkey rsa:2048 -keyout key.pem -out ca.pem -days 90 -nodes -subj '/CN=*/O=My Company Name LTD./C=US'
+	openssl req -x509 -newkey rsa:2048 -keyout key.pem -out ca.pem -days 90 -nodes -subj '/CN=*/O=Hoonio/C=US'
 	cat key.pem ca.pem > "$CERT_FOLDER/cert0.pem"
 	rm key.pem ca.pem
 fi
